d7c4da8f60172991d229e6d0442d5d79710dc4cf
Jokerkras <krasmax97@mail.ru>
2019-10-21 17:04:09 +0300

    add package domain

---
+++ src/main/java/domain/Client.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.time.LocalDate;
+
+public class Client {
+    private String name;
+    private String surname;
+    private LocalDate birthday;
+    private String email;
+}
---
+++ src/main/java/domain/Tariff.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Tariff {
+    private String title;
+    private double price;
+    private double connectionPrice;
+}
