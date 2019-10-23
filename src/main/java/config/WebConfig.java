de44c865c2b0dca79575c70f3b646354e6b687b0
Jokerkras <krasmax97@mail.ru>
2019-10-23 11:54:09 +0300

    fix gradle and gitignore

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,4 +1,15 @@
 package config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
 }
